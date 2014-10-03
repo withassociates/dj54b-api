c2826a06233db43ef5e3dced1399cd19b3f34abe
Jamie White <jamie@jgwhite.co.uk>
2014-10-03 17:52:30 +0200

    Return info from every endpoint

--- lib/dj54b/api.rb
+++ lib/dj54b/api.rb
@@ -1,4 +1,3 @@
-require 'bundler/setup'
 require 'grape'
 require 'osaka'
 
@@ -17,41 +16,65 @@ module DJ54B
       def volume
         Integer(spotify.tell("get the sound volume"))
       end
+
+      def state
+        spotify.tell("get the player state").strip
+      end
+
+      def info
+        {
+          track: {
+            url: spotify.tell("get the current track's spotify url").strip,
+            name: spotify.tell("get the current track's name").strip,
+            artist: spotify.tell("get the current track's artist").strip,
+          },
+          volume: volume,
+          state: state
+        }
+      end
+    end
+
+    before do
+      header 'Access-Control-Allow-Origin', '*'
+      header 'Access-Control-Allow-Methods', 'GET, PUT, HEAD, OPTIONS'
     end
 
     desc "Play"
     get :play do
       spotify.tell('play')
+      info
     end
 
     desc "Pause"
     get :pause do
       spotify.tell('pause')
+      info
     end
 
     desc "Next track"
     get :next do
       spotify.tell('next track')
+      info
     end
 
     desc "Current track"
     get :info do
-      {
-        name: spotify.tell("get the current track's name").strip,
-        artist: spotify.tell("get the current track's artist").strip
-      }
+      info
     end
 
     desc "Volume up"
     get :up do
-      new_volume = [volume + 10, 100].min
+      new_volume = [volume + 11, 100].min
+      $stderr.puts(new_volume)
       spotify.tell("set the sound volume to #{new_volume}")
+      info
     end
 
     desc "Volume down"
     get :down do
-      new_volume = [volume - 10, 0].max
+      new_volume = [volume - 9, 0].max
       spotify.tell("set the sound volume to #{new_volume}")
+      info
     end
   end
 end
